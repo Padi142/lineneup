6bff0e74dda018134bfa3f86e872b3f2217803da
Matyáš Krejza <matyas.krejza@seznam.cz>
2023-02-17 23:19:48 +0100

    Layout builder

--- lib/feature/lineup/view/lineup_screen.dart
+++ lib/feature/lineup/view/lineup_screen.dart
@@ -1,4 +1,3 @@
-import 'package:curved_progress_bar/curved_progress_bar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -77,124 +76,133 @@ class _MobileLineupContentState extends State<MobileLineupContent> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    widget.event.eventName,
-                    style: App.appTheme.textHeader.copyWith(fontSize: 42),
-                  ),
-                ),
-                Text(
-                  'Start - 17:00',
-                  style: App.appTheme.textTitle.copyWith(
-                      color: App.appTheme.colorSecondary, fontSize: 17),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.15,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Stack(
-                        children: [
-                          Container(
-                            width: 10,
-                            height: double.infinity,
-                            decoration: BoxDecoration(
-                                color: App.appTheme.colorInactive,
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          Container(
-                            width: 10,
-                            height: 350,
-                            decoration: BoxDecoration(
-                              color: App.appTheme.colorPrimary,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: _getArtistPictures(widget.artists),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: Material(
-                    elevation: 2,
-                    borderRadius: BorderRadius.circular(25),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: App.appTheme.colorInactive,
-                          borderRadius: BorderRadius.circular(25)),
-                      child: ArtistInfo(
-                        artist: widget.artists[chosenArtist],
-                      ),
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Column(
+          children: [
+            SizedBox(
+              height: constraints.maxHeight * 0.15,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      widget.event.eventName,
+                      style: App.appTheme.textHeader.copyWith(fontSize: 42),
                     ),
                   ),
-                )
-              ],
+                  Text(
+                    'Start - 17:00',
+                    style: App.appTheme.textTitle.copyWith(
+                        color: App.appTheme.colorSecondary, fontSize: 17),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.05,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'End - 4:00',
-                  style: App.appTheme.textTitle
-                      .copyWith(color: App.appTheme.colorPrimary, fontSize: 17),
-                ),
-                Center(
-                  child: Container(),
-                )
-              ],
+            SizedBox(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight * 0.8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.15,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Stack(
+                          children: [
+                            Container(
+                              width: 10,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: App.appTheme.colorInactive,
+                                  borderRadius: BorderRadius.circular(20)),
+                            ),
+                            Container(
+                              width: 10,
+                              height: 350,
+                              decoration: BoxDecoration(
+                                color: App.appTheme.colorPrimary,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: _getArtistPictures(widget.artists),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: Material(
+                      elevation: 2,
+                      borderRadius: BorderRadius.circular(25),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: App.appTheme.colorInactive,
+                            borderRadius: BorderRadius.circular(25)),
+                        child: ArtistInfo(
+                          artist: widget.artists[chosenArtist],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'End - 4:00',
+                    style: App.appTheme.textTitle.copyWith(
+                        color: App.appTheme.colorPrimary, fontSize: 17),
+                  ),
+                  Center(
+                    child: Container(),
+                  )
+                ],
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 
   List<Widget> _getArtistPictures(List<ArtistModel> artists) {
     return artists.map((it) {
-      return Material(
-        elevation: 5,
+      return InkWell(
         borderRadius: BorderRadius.circular(50),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(50),
-          onTap: () {
-            chosenArtist = artists.indexOf(it);
-            setState(() {});
-          },
-          child: Container(
-            width: 80,
-            height: 80,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: App.appTheme.colorPrimary),
-                image: DecorationImage(
-                    image: NetworkImage(it.artistPhoto), fit: BoxFit.fill)),
-          ),
+        onTap: () {
+          chosenArtist = artists.indexOf(it);
+          setState(() {});
+        },
+        child: Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 3,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+              shape: BoxShape.circle,
+              border: Border.all(color: App.appTheme.colorPrimary),
+              image: DecorationImage(
+                  image: NetworkImage(it.artistPhoto), fit: BoxFit.fill)),
         ),
       );
     }).toList();
@@ -209,80 +217,98 @@ class ArtistInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          Expanded(
-              flex: 2,
-              child: Row(
-                children: [
-                  Container(
-                    width: 140,
-                    height: 140,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: App.appTheme.colorPrimary),
-                        image: DecorationImage(
-                            image: NetworkImage(artist.artistPhoto),
-                            fit: BoxFit.fill)),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    artist.artistName,
-                    style: App.appTheme.textTitle.copyWith(fontSize: 30),
-                  )
-                ],
-              )),
-          Expanded(
-              child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              CurvedLinearProgressIndicator(
-                color: App.appTheme.colorPrimary,
-                strokeWidth: 6,
-                value: 0.65,
-                backgroundColor: Colors.white,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+                height: constraints.maxHeight * 0.23,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Text(
-                      DateFormat('HH:mm').format(artist.startTime),
-                      style: App.appTheme.textTitle,
+                    Row(
+                      children: [
+                        Container(
+                          height: 70,
+                          width: 70,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: NetworkImage(artist.artistPhoto),
+                                  fit: BoxFit.fill)),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Center(
+                          child: SelectableText(
+                            artist.artistName,
+                            style:
+                                App.appTheme.textTitle.copyWith(fontSize: 17),
+                            maxLines: 3,
+                          ),
+                        )
+                      ],
                     ),
-                    Container(),
-                    Text(
-                      DateFormat('HH:mm').format(artist.endTime),
-                      style: App.appTheme.textTitle,
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SelectableText(
+                          DateFormat('HH:mm').format(artist.startTime),
+                          style: App.appTheme.textTitle,
+                        ),
+                        SizedBox(
+                          width: constraints.maxWidth * 0.6,
+                          height: 6,
+                          child: LinearProgressIndicator(
+                            color: App.appTheme.colorPrimary,
+                            value: 0.65,
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                        SelectableText(
+                          DateFormat('HH:mm').format(artist.endTime),
+                          style: App.appTheme.textTitle,
+                        ),
+                      ],
                     ),
                   ],
-                ),
-              ),
-            ],
-          )),
-          Expanded(
-              flex: 6,
-              child: Text(
+                )),
+            Expanded(
+              child: SelectableText(
                 artist.artistDescription,
                 style: App.appTheme.textBody,
-              )),
-          Expanded(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              SvgButton(asset: 'assets/images/spotify-icon.svg'),
-              SvgButton(asset: 'assets/images/instagram-icon.svg'),
-              SvgButton(asset: 'assets/images/soundcloud-icon.svg'),
-              SvgButton(asset: 'assets/images/applemusic-icon.svg'),
-            ],
-          ))
-        ],
-      ),
+              ),
+            ),
+            SizedBox(
+                height: constraints.maxHeight * 0.08,
+                child: Column(
+                  children: [
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        SvgButton(asset: 'assets/images/spotify-icon.svg'),
+                        SvgButton(asset: 'assets/images/instagram-icon.svg'),
+                        SvgButton(asset: 'assets/images/soundcloud-icon.svg'),
+                        SvgButton(asset: 'assets/images/applemusic-icon.svg'),
+                      ],
+                    ),
+                  ],
+                )),
+            const SizedBox(
+              height: 15,
+            ),
+          ],
+        );
+      }),
     );
   }
 }
@@ -296,7 +322,10 @@ class SvgButton extends StatelessWidget {
     return Ink(
       child: InkWell(
         onTap: () {},
-        child: SvgPicture.asset(asset),
+        child: SvgPicture.asset(
+          asset,
+          width: 25,
+        ),
       ),
     );
   }
